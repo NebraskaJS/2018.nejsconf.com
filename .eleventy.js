5d405eff5acc2ff0492757e3503f720dde6d2775
Zach Leatherman <zachleatherman@gmail.com>
2018-04-13 09:09:41 -0500

    mixing whitespace 😱

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,17 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("assets");
+	eleventyConfig.addPassthroughCopy("assets");
 
 	return {
 		templateFormats: [
-      "md",
-      "njk",
-      "html",
-      "jstl",
-      "css"
-    ],
-    markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk",
-    dataTemplateEngine: "false",
-    passthroughFileCopy: true
-  };
+			"md",
+			"njk",
+			"html",
+			"jstl",
+			"css"
+		],
+		markdownTemplateEngine: "njk",
+		htmlTemplateEngine: "njk",
+		dataTemplateEngine: "false",
+		passthroughFileCopy: true
+	};
 };
