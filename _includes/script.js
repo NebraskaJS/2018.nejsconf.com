358b310da98f8b1c5c8104045c1bf1c0d8ed01be
William M. Riley <mikeriley20@gmail.com>
2018-05-15 12:28:11 -0500

    Page load optimizations

--- _includes/script.js
+++ _includes/script.js
@@ -6,3 +6,26 @@
 	},
 	h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
 })( document );
+
+
+// Intersection Observer
+const images = Array.from(document.querySelectorAll('.lazy'));
+
+if ('IntersectionObserver' in window) {
+	const observer = new IntersectionObserver(entries => {
+		entries.forEach(entry => {
+			if (entry.intersectionRatio > 0) {
+				entry.target.classList.add('load');
+				observer.unobserve(entry.target);
+			}
+		});
+	});
+
+	images.forEach(image => {
+		observer.observe(image);
+	});
+} else {
+	images.forEach(image => {
+		image.target.classList.add('load');
+	});
+}
